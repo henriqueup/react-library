6dacc6184fde7d8e85faaa68e7be01996703ecf7
Henrique Pessoa <titepessoa@gmail.com>
2022-08-27 18:47:08 -0300

    fix rollup

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -4,35 +4,39 @@ import resolve from "@rollup/plugin-node-resolve";
 import scss from "rollup-plugin-scss";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
 
-export default [
-  {
-    input: "./src/index.ts",
-    output: [
-      {
-        file: "dist/index.js",
-        format: "cjs",
-      },
-      {
-        file: "dist/index.es.js",
-        format: "es",
-        exports: "named",
-      },
-    ],
-    plugins: [
-      scss({
-        output: true,
-        failOnError: true,
-        outputStyle: "compressed",
-      }),
-      babel({
-        exclude: "node_modules/**",
-        presets: ["@babel/preset-react"],
-      }),
-      external(),
-      resolve(),
-      typescript(),
-      terser(),
-    ],
-  },
-];
+const config = {
+  input: "./src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es",
+      exports: "named",
+    },
+  ],
+  plugins: [
+    scss({
+      output: true,
+      failOnError: true,
+      outputStyle: "compressed",
+    }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-react"],
+    }),
+    external(),
+    resolve(),
+    typescript(),
+    terser(),
+  ],
+};
+
+export default config;
