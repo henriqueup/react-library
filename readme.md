97c5e8e3ddae94e6e4b71f331c8682fbf500a4d1
Henrique Pessoa <titepessoa@gmail.com>
2022-08-27 18:33:42 -0300

    initial commit, ui package base

---
+++ readme.md
@@ -0,0 +1,10 @@
+## About
+
+React library with UI components package
+Intentions are to expand the library with other packages eventually
+
+### UI
+
+A package that contains React components and their storybooks
+
+Created following the guide on https://dev.to/iamrishupatel/how-to-create-a-react-component-library-using-storybook-typescript-scss-and-rollup-4pin
---
+++ ui/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant: "primary" | "danger";
+  shape?: "rounded";
+}
+
+const Button: React.FC<ButtonProps> = ({ variant, shape, children, ...props }) => {
+  const classNames = `btn btn-${variant} btn-${shape}`;
+
+  return (
+    <button className={classNames} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
